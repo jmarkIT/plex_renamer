4f79acba956c38f59daa0ab201a8e6c7eefa4609
James Mark <james.david.mark@gmail.com>
2019-12-30 21:38:51 -0600

    Technically this works now

--- plex_renamer.py
+++ plex_renamer.py
@@ -6,17 +6,21 @@ files = []
 for file in glob("*"):
     files.append(file)
 
-template = input("> ")
+for file in files:
+    print(file)
+template = input("What's the name of the show?\n")
 
 for file in files:
         answer = 'n'
         system('clear')
         while answer != 'y':
-            print(template)
+            print(file)
             season = input("What season?\n")
             episode = input("What episode?\n")
-            new_file_name = f"{template} s{season}e{episode}"
+            file_type = input("Wait, whats the file extension?\n")
+            new_file_name = f"{template} s{season}e{episode}.{file_type}"
             print(new_file_name)
-            answer = input("Is this what you want? y/N")
+            answer = input("Is this what you want? y/N\n")
+        move(file, new_file_name)
 
     
